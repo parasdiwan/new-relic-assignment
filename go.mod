f74db0e0419dc4863d61b81a50aed00bf3d493e1
Paras Diwan <prs.diwan@gmail.com>
2021-11-09 23:57:16 +0530

    Add tcp connection

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/new-relic-assignment
+
+go 1.14
--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
 func main() {
 	fmt.Println("hello world!")
+	listener, err := net.Listen("tcp", "127.0.0.1:4000")
+	if err != nil {
+		fmt.Println("there has been a problem")
+		fmt.Println(err)
+	}
+	connection, err := listener.Accept()
+	if err != nil {
+		fmt.Println("there has been a problemo accepting connections!")
+	}
+	isInputValid := true
+	for isInputValid {
+		var inputString, _ = bufio.NewReader(connection).ReadString('\n')
+		fmt.Println(inputString)
+		// TODO trim input
+		isInputValid = checkInputValidity(inputString)
+	}
+}
+
+func checkInputValidity(inputString string) bool {
+	return true
 }
