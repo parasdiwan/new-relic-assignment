9dc318d7eccb07663462802ef90049b065ad576a
Paras Diwan <prs.diwan@gmail.com>
2021-11-15 05:37:58 +0530

    Create logger

--- fileLogger.go
+++ fileLogger.go
@@ -1,5 +1,16 @@
 package main
 
-func StartLogger() {
+import (
+	"log"
+	"os"
+)
 
+func StartLogger() *os.File {
+	f, err := os.OpenFile("numbers.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetFlags(0)
+	log.SetOutput(f)
+	return f
 }
